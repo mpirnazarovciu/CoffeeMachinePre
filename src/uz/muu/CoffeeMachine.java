9c9dfc279fa34df1843113b8d80ca6228a7abb0d
Muslimbek Pirnazarov <muslimbekpirnazarov@Muslimbeks-MacBook-Pro.local>
2023-03-13 14:32:54 +0500

    Initial commit

---
+++ src/uz/muu/CoffeeMachine.java
@@ -0,0 +1,34 @@
+package uz.muu;
+
+public class CoffeeMachine {
+
+    public void addCoffee(String name, double price, double coffeeAmount, double milkAmount, double waterAmount) {
+    }
+
+    public double getPrice(String coffeeName) {
+        return 0.0;
+    }
+
+    public void rechargeCard(int cardId, double credit) {
+
+    }
+
+    public double getCredit(int cardId) {
+        return 0.0;
+    }
+
+    public int sell(String coffeeName, int cardId) {
+        return 0;
+    }
+
+    public void refillProduct(String productName, int amount) {
+    }
+
+    public boolean availableCoffee(String coffeeName) {
+        return false;
+    }
+
+    public double availableProduct(String productName) {
+        return -1;
+    }
+}
